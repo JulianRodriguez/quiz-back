6a8de3090ea8b6249f8c60773a055facbc60679a
Julito <Julito@mbp-de-julian.home>
2018-05-17 13:09:07 +0200

    clase user

---
+++ src/main/java/com/bgg/quizback/model/User.java
@@ -0,0 +1,37 @@
+package com.bgg.quizback.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Integer idUser;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(unique = true, nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+	
+	@ManyToMany(fetch = FetchType.LAZY, mappedBy = Curso.FIELD_USER)
+	private List<Curso> curso;
+
+}
