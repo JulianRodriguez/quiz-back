b88d990421f13e7892cae532cd7bfe15a9f05680
Julito <Julito@mbp-de-julian.home>
2018-05-17 13:22:05 +0200

    clase dificultad

---
+++ src/main/java/com/bgg/quizback/model/Dificultad.java
@@ -0,0 +1,25 @@
+package com.bgg.quizback.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Dificultad {
+	
+	@Id
+	@GeneratedValue
+	private Integer idDificultad;
+	
+
+}
