3f1cc65336a4b8d07aeb37c82bda54b916572bf0
Julito <Julito@mbp-de-julian.home>
2018-05-17 13:22:18 +0200

    clase respuesta

---
+++ src/main/java/com/bgg/quizback/model/Respuesta.java
@@ -0,0 +1,23 @@
+package com.bgg.quizback.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Respuesta {
+
+	@Id
+	@GeneratedValue
+	private Integer idRespuesta;
+}
