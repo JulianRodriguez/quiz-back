f93138992c86604fcb79072a750b3fe159b4f182
Julito <Julito@mbp-de-julian.home>
2018-05-21 11:21:13 +0200

    realaciones

--- src/main/java/com/bgg/quizback/model/Tag.java
+++ src/main/java/com/bgg/quizback/model/Tag.java
@@ -2,12 +2,10 @@ package com.bgg.quizback.model;
 
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
