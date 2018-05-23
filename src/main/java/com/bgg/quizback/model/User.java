c19e4990a4f6c7265b3f1aad3cffc83de5143c78
Julito <Julito@localhost>
2018-05-23 17:53:39 +0200

    user

--- src/main/java/com/bgg/quizback/model/User.java
+++ src/main/java/com/bgg/quizback/model/User.java
@@ -25,7 +25,7 @@ public class User {
 	@Column(nullable = false)
 	private String name;
 
-	@Column(unique = true, nullable = false)
+	@Column(nullable = false)
 	private String email;
 
 	@Column(nullable = false)
