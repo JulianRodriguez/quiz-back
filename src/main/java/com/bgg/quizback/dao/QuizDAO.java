67dc2aecbd5b4b26f9175449796d572547a9659f
Julito <Julito@mbp-de-julian.home>
2018-05-30 12:49:32 +0200

    dao quiz

---
+++ src/main/java/com/bgg/quizback/dao/QuizDAO.java
@@ -0,0 +1,15 @@
+package com.bgg.quizback.dao;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bgg.quizback.model.Quiz;
+import com.bgg.quizback.model.User;
+
+@Repository
+public interface QuizDAO extends PagingAndSortingRepository<Quiz, Integer>{
+	
+	Quiz findByIdQuiz(Integer idQuiz);
+
+
+}
