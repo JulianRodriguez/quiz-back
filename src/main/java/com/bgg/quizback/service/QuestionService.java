a103926071a5036dc7b1d1682e8a0c5f8d4ca254
Julito <Julito@mbp-de-julian.home>
2018-05-31 13:41:08 +0200

    servicio question

--- src/main/java/com/bgg/quizback/service/QuestionService.java
+++ src/main/java/com/bgg/quizback/service/QuestionService.java
@@ -1,7 +1,14 @@
 package com.bgg.quizback.service;
 
+import java.util.Set;
+
 import com.bgg.quizback.model.Question;
 
 public interface QuestionService extends AbstractCRUBService<Question, Integer>{
+	
+	Question findByIdQuestion(Integer id);
+	void delete(Integer id);
+	void saveQuizQuestion(Integer idquestion, Integer idquiz);
+
 
 }
--- src/main/java/com/bgg/quizback/service/QuestionServiceImpl.java
+++ src/main/java/com/bgg/quizback/service/QuestionServiceImpl.java
@@ -1,5 +1,75 @@
 package com.bgg.quizback.service;
 
-public class QuestionServiceImpl {
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
+import org.springframework.aop.aspectj.AspectJAdviceParameterNameDiscoverer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.transform.impl.AddPropertyTransformer;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.bgg.quizback.dao.QuestionDAO;
+import com.bgg.quizback.dao.QuizDAO;
+import com.bgg.quizback.model.Question;
+import com.bgg.quizback.model.Quiz;
+
+@Service
+public class QuestionServiceImpl implements QuestionService{
+
+	@Autowired
+	QuestionDAO questiondao;
+	
+	@Autowired
+	QuizDAO quizdao;
+	
+	@Override
+	public Question create(Question t) {
+		return questiondao.save(t);
+	}
+
+	@Override
+	public void update(Question t) {
+		questiondao.save(t);
+	}
+
+	@Override
+	public Optional<Question> findById(Integer id) {
+		return questiondao.findById(id);
+	}
+
+	@Override
+	public Set<Question> findAll(Pageable p) {
+		int page = p.getPageNumber();
+		int size = p.getPageSize();
+		return questiondao.findAll(PageRequest.of(page, size)).stream().collect(Collectors.toSet());
+	}
+
+	@Override
+	public void delete(Question t) {
+		questiondao.delete(t);
+		
+	}
+
+	@Override
+	public Question findByIdQuestion(Integer id) {
+		return questiondao.findByIdQuestion(id);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		questiondao.deleteById(id);
+		
+	}
+	
+	@Override
+	public void saveQuizQuestion(Integer idquestion, Integer idquiz) {
+		final Quiz quiz = quizdao.findByIdQuiz(idquiz);
+		final Question question = questiondao.findByIdQuestion(idquestion);
+	
+	}
 
 }
